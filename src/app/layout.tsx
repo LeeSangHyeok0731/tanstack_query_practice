eaf8d4d9c52e3b0a1b3034fcd76a656f04b61b9f
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-10 21:16:59 +0900

    우류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <html lang="en">
