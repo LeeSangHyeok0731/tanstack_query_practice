506bafcf45581e954ef0a4bfd95730bb430e8ce7
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-11 10:47:38 +0900

    커스텀 훅 추가

--- src/app/ApiPage/index.tsx
+++ src/app/ApiPage/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import useGetApi from "../hook/useGetApi";
 
 type ApiData = {
   id: number;
@@ -20,22 +20,15 @@ const DataResponse = styled.div`
 `;
 
 export default function API() {
-  const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-
-    return response.json();
-  };
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["data"],
-    queryFn: fetchData,
-  });
+  const { data, isLoading, error } = useGetApi();
 
   if (isLoading) return <>데이터 불러오는중....</>;
   if (error) return <>데이터 불러오기 실패 ㅠㅠ</>;
 
   return (
     <div>
-      api 불러오기 성공! <br />
+      {data && <p> api 불러오기 성공!</p>}
+      <br />
       {data.map((x: ApiData, index: number) => {
         return (
           <DataResponse key={index}>
