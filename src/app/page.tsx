cb83bfcd08ad2788db870341c41b6407f4d08779
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-12 23:22:37 +0900

    <Mutation />추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import API from "./ApiPage/getApiAll";
+import Mutation from "./ApiPage/mutationApi";
 
 export default function Home() {
   return (
     <>
+      <Mutation />
       <API />
     </>
   );
