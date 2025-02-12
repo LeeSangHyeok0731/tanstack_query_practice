9f83f193fd99306c602bc9d3fd6eb9d3f6391ef7
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-12 23:22:19 +0900

    게시글 등록 완성

--- src/app/ApiPage/mutationApi/index.tsx
+++ src/app/ApiPage/mutationApi/index.tsx
@@ -1,5 +1,49 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+
 export default function Mutation() {
-  return <></>;
+  const fetchData = async ({
+    title,
+    body,
+    userId,
+  }: {
+    title: string;
+    body: string;
+    userId: number;
+  }) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({ title, body, userId }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    return response.json();
+  };
+
+  const mutation = useMutation({
+    mutationFn: fetchData,
+    onSuccess: (data) => {
+      console.log("성공:", data);
+    },
+    onError: (error) => {
+      console.error("오류:", error);
+    },
+  });
+
+  return (
+    <button
+      onClick={() =>
+        mutation.mutate({
+          title: "이상혁",
+          body: "안녕하세요",
+          userId: 1412,
+        })
+      }
+    >
+      새로은 글 만들기
+    </button>
+  );
 }
