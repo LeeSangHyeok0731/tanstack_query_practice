22189ad52b78c88f80f8f0bda07a2e28f7931b85
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-14 00:16:07 +0900

    refetch 함수 삭제

--- src/app/hook/useGetApi.tsx
+++ src/app/hook/useGetApi.tsx
@@ -7,12 +7,12 @@ export default function useGetApi() {
     return response.json();
   };
 
-  const { data, isLoading, error, refetch } = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ["data"],
     queryFn: fetchData,
     staleTime: 1000 * 60 * 0,
     gcTime: 1000 * 60 * 0,
   });
 
-  return { data, isLoading, error, refetch };
+  return { data, isLoading, error };
 }
